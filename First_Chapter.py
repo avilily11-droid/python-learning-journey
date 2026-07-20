b866ac4f80c35e7b3da0f7a99967e2bf82c64854
Avi <avi11lily@gmail.com>
2026-07-20 21:52:34 +0530

    Update

--- First_Chapter.py
+++ First_Chapter.py
@@ -3,7 +3,8 @@ some_variable = 42
 some_other_variable = "This is a string"
 additional_variable = [1, 2, 3, 4, 5]
 additional_variable.append(6)
+some_variable += 1
 
 print(some_variable)
-     
+print(some_variable * 2)     
 
