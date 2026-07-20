1975c604c00299c38f313ae1021e50e0bb231317
Avi <avi11lily@gmail.com>
2026-07-20 21:07:53 +0530

    First chapter upload

---
+++ First_Chapter.py
@@ -0,0 +1,9 @@
+print("Hello, World!")  
+some_variable = 42
+some_other_variable = "This is a string"
+additional_variable = [1, 2, 3, 4, 5]
+additional_variable.append(6)
+
+print(some_variable)
+     
+
