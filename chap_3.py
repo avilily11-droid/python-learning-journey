224fb6c808af2879e9e27742a38880b25763e8c6
Avi <avi11lily@gmail.com>
2026-08-08 01:20:34 +0530

    lecture3

---
+++ chap_3.py
@@ -0,0 +1,15 @@
+str1 = "avi"
+str2= "  singwalia"
+
+print(str1+str2)
+
+len1=len(str1)
+len2=len(str2)
+
+print ("the length of str1 is:",len1,"/n", "the length of str2 is;",len2)
+
+ch = str1[0]
+ch1= str2[6]
+ch2 = str1[2]
+
+print("character ;",ch,"\n","character b;",ch1,"\n","character a;",ch2)
