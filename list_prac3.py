40fc291493ca63146819b1dc5bdf11608220de97
Avi <avi11lily@gmail.com>
2026-08-09 01:13:13 +0530

    list function

---
+++ list_prac3.py
@@ -0,0 +1,31 @@
+# list methods 
+# list slicing
+
+marks = [18,39,4,49,54,4]
+print (type(marks))
+print(marks)
+marks[3]= 58
+
+print(marks)
+
+marks.append(44)
+
+print(marks)
+
+marks.sort()
+print(marks)
+
+marks.sort(reverse=True)
+print(marks)
+
+marks.insert(4,78)
+print(marks)
+
+marks.pop()
+print(marks)
+
+
+marks.remove(4)
+print(marks)
+
+
