ebf17bd4829f34b48107d0b18941ef4d4dc02694
Avi <avi11lily@gmail.com>
2026-08-08 19:32:21 +0530

    if,else(use)

---
+++ lec2_part2.py
@@ -0,0 +1,44 @@
+number1 =34
+number2 =92
+number3 =27
+
+if (number1/2) == int(number1/2):
+    print("no.1 is even")
+else:
+    print("no.1 is odd")
+if (number2/2) == int(number2/2):
+    print("no.2 is even")
+else:
+    print("no.2 is odd")
+if (number3/2) == int(number3/2):
+    print("no.3 is even")
+else: 
+    print("no.3 is odd")
+
+
+
+a= 34
+b=56
+c=45.4050509300000000905855555558558
+
+if (a>b) and (a>c):
+    print("greatest no. is a")
+
+elif (b>c) and (b>a):
+        print( "b is greatest no.")
+
+else:
+        print("c is greatest no among all ")
+
+
+value = int(input("enter any no:"))
+
+
+print(value)
+
+if (value %7 ) == 0:
+     print("no. is multiple of 7")
+
+else:
+     print(" no. is very bad ")
+
