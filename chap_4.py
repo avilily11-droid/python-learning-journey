f32c40b4bd1ead9682744bc6e3994b977ab55493
Avi <avi11lily@gmail.com>
2026-08-10 01:50:41 +0530

    dictionary & sets

---
+++ chap_4.py
@@ -0,0 +1,78 @@
+
+
+dict = { "table" : [ "lists of facts and figure", "a piece of furniture" ],
+           "cat" : "A small animal",
+        }
+
+print(dict)
+print(type(dict))
+
+student = {"java","javascript","c++","c",
+         "python","java","python","c++","java","python",
+         "c"}
+
+print(len(student))
+print(type(student))
+print(student)
+
+marks = {}
+
+x = int(input("enter phy"))
+marks.update ({"phy":x})
+
+y= int(input("enter chem"))
+marks.update({"chem":y})
+
+z= int(input("enter maths"))
+marks.update({"maths":z})
+
+
+print(marks)
+
+set = {(9,9.0), 34
+
+            }
+
+print (set)
+
+
+
+set.add(8)
+print(set)
+set.remove(34)
+print(set)
+set.pop()
+print(set)
+
+set2= {5,4,5,25,28,2,9,8}
+
+set.union(set2)
+print(set)
+
+set.intersection(set2)
+print(set)
+
+print(len(set))
+
+
+dict4= {
+        "study" : "padhai",
+        "avi"  : "[23,84,758,8484]"
+
+}
+
+dict4.update({"rashi":"girl"})
+print(dict4)
+dict4.keys()
+print(dict4)
+dict4.values()
+print(dict4)
+
+print(type(dict4))
+print(len(dict4))
+
+dict4.clear()
+print(dict4)
+
+
+#            LECTURE -4 ENDED OF PHYTHON SERIES DICTIONARY AND SETS
